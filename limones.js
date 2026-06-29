f29a3503406801247b14b2704e59107a6d73223d
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-06-29 10:40:30 -0500

    mover a la izquierda

--- limones.js
+++ limones.js
@@ -4,11 +4,13 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
 
 function iniciar(){
     dibujarsuelo();
     dibujarpersonaje();
 }
+
 function dibujarsuelo(){
     ctx.fillStyle="green";
     ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
@@ -16,5 +18,19 @@ function dibujarsuelo(){
 
 function dibujarpersonaje(){
     ctx.fillStyle="orange";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+function moverizquierda(){
+    personajeX=personajeX-10;
+    actualizarpantalla();
+}
+function actualizarpantalla(){
+    limpiarcanva();
+    dibujarsuelo();
+    dibujarpersonaje();
+}
+
+function limpiarcanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
