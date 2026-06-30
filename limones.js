ae9f1ee3a1b8afa13afb5297f321bc160ca6cc71
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-06-30 14:24:11 -0500

    agregar bajarlimon

--- limones.js
+++ limones.js
@@ -4,11 +4,18 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+const ANCHO_LIMON=20;
+const ALTURA_LIMON=20;
+
 let personajeX=canvas.width/2;
+
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){
     dibujarsuelo();
     dibujarpersonaje();
+    dibujarlimon();
 }
 
 function dibujarsuelo(){
@@ -35,8 +42,19 @@ function actualizarpantalla(){
     limpiarcanva();
     dibujarsuelo();
     dibujarpersonaje();
+    dibujarlimon();
 }
 
 function limpiarcanva(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibujarlimon(){
+    ctx.fillStyle="lime";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
+}
+
+function bajarlimon(){
+    limonY=limonY+10;
+    actualizarpantalla();
 }
